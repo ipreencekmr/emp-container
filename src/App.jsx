09420a530215fd8510b197634b07112ebb4ac34d
Prince Kumar Sharma <ipreencekmr@gmail.com>
2023-08-03 14:36:23 +0530

    linting enforced

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+// eslint-disable-next-line import/no-unresolved
 import Home from "emp_home/Home";
 
 const App = () => (
-  <main className="mt-6">
-    <hr></hr>
-    <Home></Home>
-  </main>
+    <main className="mt-6">
+        <hr></hr>
+        <Home></Home>
+    </main>
 );
 
 const globalApp = document.getElementById("app");
